b55d2233416a23375356bfdcd3c9e1423eca20b2
Dnyanesh Chavan <dnyaneshchavan2001@gmail.com>
2024-02-05 10:56:09 +0530

    update candidate page
    jaywant upadated candidate page

--- src/Pages/Candidate.js
+++ src/Pages/Candidate.js
@@ -5,93 +5,58 @@ function Candidate() {
 
     return (
 
-        <div className='container'>
+        <div className='container1'>
             <div className='header'>
                 <div className='text'>Sign Up</div>
                 <div className='underline' ></div>
             </div>
             <div className='Inputs'>
                 <div className='Input'>
-                    {/* <img src={img1} alt='' /> */}
-                    <input type='text' inputMode='numeric' placeholder='Aadhar Number' />
+                    <input id="ip" type='text' inputMode='numeric' placeholder='Aadhar Number' />
+                    <input id="ip" type='text' inputMode='text' placeholder='First Name' />
+                    <input id="ip" type='text' inputMode='text' placeholder='Middle Name' />
+                </div>
+ 
+                <div className='Input'>
+                    <input id="ip" type='text' inputMode='text' placeholder='Last Name' />
+                    <input  id="ip" type='date' inputMode='text' placeholder='Date of Birth' />
+                    <input id="ip" type='number' placeholder='Annual Income' />
                 </div>
 
                 <div className='Input'>
-                    <img src='' alt='' />
-                    <input type='text' placeholder='First Name' />
+                         <input id="ip" type='text' placeholder='Party Name' />
+                         <input id="ip" type='number' placeholder='Ward Number' />
+                         <input id="ip" type='number' placeholder='Criminal cases' />
                 </div>
 
                 <div className='Input'>
-                    <img src='' alt='' />
-                    <input type='text' placeholder='Middle Name' />
-                </div>
-                <div className='Input'>
-                    <img src='' alt='' />
-                    <input type='text' placeholder='Last Name' />
+                    <input id="ip" type='text' placeholder='Address' />
+                    <input id="ip" type='number' placeholder='Pincode' />  
+                    <input id="ip" type='password' name="password" placeholder='Password' />
+                    
                 </div>
 
                 <div className='Input'>
-                    <img src='' alt='' />
-                    <input type='date' placeholder='Date of Birth' />
-                </div>
-
-
-
-                <div className='Input'>
-                    Gender :
-                    <img src='' alt='' />
-                    <div>
-                        <span>
-                            <input type='radio' name="Gender" value="Male" /> Male
-                            <input type='radio' name="Gender" value="Female" /> Female
-                            <input type='radio' name="Gender" value="Other" /> Other
-                        </span>
-                    </div>
-                </div>
-
-                <div className='Input'>
-                    <img src='' alt='' />
-                    <input type='number' placeholder='Annual Income' />
-                </div>
-
-                <div className='Input'>
-                    <img src='' alt='' />
-                    <input type='number' placeholder='Criminal cases' />
-                </div>
-
-                <div className='Input'>
-                    <img src='' alt='' />
-                    <input type='text' placeholder='Address' />
-                </div>
-
-                <div className='Input'>
-                    <img src='' alt='' />
-                    <input type='number' placeholder='Pincode' />
-                </div>
-
-                <div className='Input' >
-                    <input type='text' placeholder='Party Name' />
-                </div>
-
-                <div className='Input'>
-                    <img src='' alt='' />
-                    <input type='number' placeholder='Ward Number' />
-                </div>
-
-                <div className='Input'>
-                    <img src='' alt='' />
-                    <input type='password' name="password" placeholder='Password' />
-                </div>
-                <div className='Input'>
-                    <input type='password' name='password' placeholder='Re-Enter Password' />
-                </div>
-
-                <div className='submit-container'>
-                    <div className='submit'>Submit</div>
-                    <div className='submit'>Login</div>
-                </div>
+                         <input id="ip" type='password' name='password' placeholder='Re-Enter Password' />
+                     
+                         <label className='gender'>
+                            Select your gender :
+                            <select >
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                            </select>
+                        </label>
+                         
+               
+                 </div>
             </div>
-        </div>
+
+            <div className='btnclass'>
+                <button className='btn'>Submit</button>
+                <button className='btn'>Login</button>
+            </div>
+          </div>
     );
 }
 
